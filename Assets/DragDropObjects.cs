db3b336b6d2c45ec913f97bb00cfc5f1471df73c
Andrej Kuznecov <aku@tiatechnology.com>
2020-05-20 17:38:16 +0300

    Update InsertionSortScene

--- Assets/DragDropObjects.cs
+++ Assets/DragDropObjects.cs
@@ -45,6 +45,21 @@ public class DragDropObjects : MonoBehaviour
         //0.764151f, 0.08950541f, 0.08290318f, 1f red
         Reticle = GameObject.Find("GvrReticlePointer (1)");
     }
+    public void PaintItRed()
+    {
+        IsHeld = false;
+        GameObject.Find("Cube").GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        GameObject.Find("Cube (1)").GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        GameObject.Find("Cube (2)").GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        GameObject.Find("Cube (3)").GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        GameObject.Find("Cube (5)").GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        GameObject.Find("Cube (8)").GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        GameObject.Find("Cube (8)").GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        //0.2274491f, 0.6509434f, 0.1443129f, 1f green
+        //0.9056604f, 0.5189343f, 0.2947668f, 1f orange
+        //0.764151f, 0.08950541f, 0.08290318f, 1f red
+        Reticle = GameObject.Find("GvrReticlePointer (1)");
+    }
 
     // Update is called once per frame
     void Update()
